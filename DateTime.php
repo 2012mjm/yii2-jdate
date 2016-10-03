9208aa1861590a19ef4aca544f0c12aa534c7744
2007mjm <2007mjm@gmail.com>
2016-10-03 13:57:24 +0330

    - Add toGregorianDate function

--- DateTime.php
+++ DateTime.php
@@ -7,6 +7,7 @@ use yii\base\InvalidParamException;
  * Jalali date & time.
  * @author Mohammad Mahdi Gholomian.
  * @copyright 2014 mm.gholamian@yahoo.com
+ * @developed by Mohammad Javad Masoudian <info@mjm3d.ir>
  */
 class DateTime
 {
@@ -170,6 +171,64 @@ class DateTime
 		return array($jy, $jm, $jd); 
 
 	}
+
+    /**
+     * Jalali to Gregorian Conversion
+     * Copyright (C) 2000  Roozbeh Pournader and Mohammad Toossi
+     * Edited by Mohammad Javad Masoudian <info@mjm3d.ir>
+     */
+    public static function toGregorianDate($j_y, $j_m, $j_d)
+    {
+
+        $g_days_in_month = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+        $j_days_in_month = array(31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29);
+
+        $jy = $j_y-979;
+        $jm = $j_m-1;
+        $jd = $j_d-1;
+
+        $j_day_no = 365*$jy + $this->div($jy, 33)*8 + $this->div($jy%33+3, 4);
+        for ($i=0; $i < $jm; ++$i)
+            $j_day_no += $j_days_in_month[$i];
+
+        $j_day_no += $jd;
+
+        $g_day_no = $j_day_no+79;
+
+        $gy = 1600 + 400*$this->div($g_day_no, 146097);
+        $g_day_no = $g_day_no % 146097;
+
+        $leap = true;
+        if ($g_day_no >= 36525) {
+            $g_day_no--;
+            $gy += 100*$this->div($g_day_no,  36524);
+            $g_day_no = $g_day_no % 36524;
+
+            if ($g_day_no >= 365)
+                $g_day_no++;
+            else
+                $leap = false;
+        }
+
+        $gy += 4*$this->div($g_day_no, 1461);
+        $g_day_no %= 1461;
+
+        if ($g_day_no >= 366) {
+            $leap = false;
+
+            $g_day_no--;
+            $gy += $this->div($g_day_no, 365);
+            $g_day_no = $g_day_no % 365;
+        }
+
+        for ($i = 0; $g_day_no >= $g_days_in_month[$i] + ($i == 1 && $leap); $i++)
+            $g_day_no -= $g_days_in_month[$i] + ($i == 1 && $leap);
+        $gm = $i+1;
+        $gd = $g_day_no+1;
+
+        return array($gy, $gm, $gd);
+
+    }
 
 	function div($a,$b) {
 		return (int) ($a / $b);
